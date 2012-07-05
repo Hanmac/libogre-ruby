9abaf4f57f17051baf96b04ae2d5cb1c0ef21f96
Hanmac <hanmac@gmx.de>
2012-07-05 19:46:59 +0200

    ADD added viewport and look at methods to Camera

--- ext/ogrecamera.cpp
+++ ext/ogrecamera.cpp
@@ -29,6 +29,8 @@ macro_attr(AutoAspectRatio,bool)
 
 macro_attr(UseRenderingDistance,bool)
 
+singlereturn(getViewport)
+
 singlereturn(getUp)
 singlereturn(getRight)
 
@@ -66,6 +68,17 @@ VALUE _pitch(VALUE self,VALUE angle)
 	return self;
 }
 
+VALUE _lookAt(int argc,VALUE *argv,VALUE self)
+{
+	VALUE x,y,z;
+	rb_scan_args(argc, argv, "12",&x,&y,&z);
+	if(argc == 3)
+		_self->lookAt(NUM2DBL(x),NUM2DBL(y),NUM2DBL(z));
+	else
+		_self->lookAt(wrap<Ogre::Vector3>(x));
+	return self;
+}
+
 }
 }
 
@@ -92,10 +105,14 @@ void Init_OgreCamera(VALUE rb_mOgre)
 	rb_cOgreCamera = rb_define_class_under(rb_mOgre,"Camera",rb_cOgreFrustum);
 	rb_undef_alloc_func(rb_cOgreCamera);
 
+	rb_define_method(rb_cOgreCamera,"viewport",RUBY_METHOD_FUNC(_getViewport),0);
+
 	rb_define_method(rb_cOgreCamera,"roll",RUBY_METHOD_FUNC(_roll),1);
 	rb_define_method(rb_cOgreCamera,"yaw",RUBY_METHOD_FUNC(_yaw),1);
 	rb_define_method(rb_cOgreCamera,"pitch",RUBY_METHOD_FUNC(_pitch),1);
 	
+	rb_define_method(rb_cOgreCamera,"look_at",RUBY_METHOD_FUNC(_lookAt),-1);
+
 	rb_define_attr_method(rb_cOgreCamera,"position",_getPosition,_setPosition);
 	rb_define_attr_method(rb_cOgreCamera,"direction",_getDirection,_setDirection);
 	rb_define_attr_method(rb_cOgreCamera,"orientation",_getOrientation,_setOrientation);
